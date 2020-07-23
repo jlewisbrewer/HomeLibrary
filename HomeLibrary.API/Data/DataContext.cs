2531b1eb32c2a368d7b441c9da715bde1b1137f6
Jason Brewer <jbrewer@pdx.edu>
2020-07-23 15:54:35 -0700

    Added database context, values controller

---
+++ HomeLibrary.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using HomeLibrary.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeLibrary.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ HomeLibrary.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace HomeLibrary.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+    }
+}
