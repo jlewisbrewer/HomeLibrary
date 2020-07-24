453859f97fc05e702195bb4438b3dc2632592aed
Jason Brewer <jbrewer@pdx.edu>
2020-07-24 03:27:50 -0700

    Added register functionality

---
+++ HomeLibrary.API/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using HomeLibrary.API.Data;
+using HomeLibrary.API.Dtos;
+using HomeLibrary.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeLibrary.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
