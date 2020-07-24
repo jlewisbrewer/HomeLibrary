b6fd9a97d3a9ed33319af2fdfc61281a7d00376c
Jason Brewer <jbrewer@pdx.edu>
2020-07-23 20:31:03 -0700

    Added AuthRepository class and interface

---
+++ HomeLibrary.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using HomeLibrary.API.Models;
+
+namespace HomeLibrary.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
+    }
+}
